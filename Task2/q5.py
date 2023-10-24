44fcd8c5775bbfa69fc458c10004e18cdf22dcbe
UtkarshKumarJha <145829192+UtkarshKumarJha@users.noreply.github.com>
2023-10-24 20:04:28 +0530

    Update q5.py

--- Task2/q5.py
+++ Task2/q5.py
@@ -3,3 +3,7 @@ def decimal_to_binary_recursive(decimal):
         return "0b0"
     else:
         return decimal_to_binary_recursive(decimal // 2) + str(decimal % 2)
+decimal_str = input("Enter the decimal: ")
+decimal = int(decimal_str)
+ans = decimal_to_binary_recursive(decimal)
+print(ans)            
