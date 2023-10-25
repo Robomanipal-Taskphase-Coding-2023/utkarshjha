0f6b8acbcf75809be24c9938e65b680a5a91b4f1
UtkarshKumarJha <145829192+UtkarshKumarJha@users.noreply.github.com>
2023-10-25 15:59:19 +0530

    Update q10.py

--- Task2/q10.py
+++ Task2/q10.py
@@ -1,14 +1,21 @@
 def parse_encoded_string(encoded_string):
-    parts = encoded_string.split('0+')
-    
-    first_name = parts[0]
-    last_name = parts[1]
-    user_id = parts[2]
-    
-    result_dict = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "id": user_id
-    }
-    
-    return result_dict
+    parts = encoded_string.split('000')
+
+    if len(parts) >= 3:
+        first_name = parts[0]
+        last_name = parts[1]
+        user_id = parts[2]
+
+        result_dict = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "id": user_id
+        }
+
+        return result_dict
+    else:
+        return {"error": "Invalid input format"}
+
+encoded_string = input("Enter the string: ")
+decoded_string = parse_encoded_string(encoded_string)
+print(decoded_string)
